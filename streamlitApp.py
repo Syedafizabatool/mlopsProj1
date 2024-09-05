856268a6e5006c587d8a0c9ba011816f2c2b1594
Syedafizabatool <syedafizabatool36@gmail.com>
2024-09-05 09:31:32 +0000

    fixedbugs

--- streamlitApp.py
+++ streamlitApp.py
@@ -1,38 +1,42 @@
-import pandas as pd
+import pandas as pd 
 import joblib
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import streamlit as st
 
-model = joblib.load("liveModelV1.pk1")
+model = joblib.load("liveModelV1.pkl")
 
-data= pd.read_csv('mobile_price_range_data.csv')
-X=data.iloc[:,:-1]
-y=data.iloc[:,:-1]
+data = pd.read_csv('mobile_price_range_data.csv')
 
-X_train ,X_test, y_train ,y_test = train_test_split (X,y,test_size=0.2,random_state=42)
-#Make predictions for X_test set
-y_pred =model.predict(X_test)
+x = data.iloc[:, :-1]
+y = data.iloc[:, -1]
 
-#Calculate Accuray
-accuracy= accuracy_score (y_test,y_pred)
+x_train,x_test,y_train,y_test = train_test_split(x , y,test_size = 0.2,random_state=42)
 
-#Page title
+y_pred = model.predict(x_test)
+
+# calculate accuracy
+accuracy = accuracy_score(y_test,y_pred)
+
+#page title
 st.title("Model Accuracy and Real-Time Prediction")
 
-#Display Accuarcy
-st.write(f"Model{accuracy}")
-#Real time predictions based on user inputs
-st.header("Real-Time Prediction")
-input_data =[]
-for col in X_test.columns:
-     input_value=st.number_input(f'Input for feature{col}',value='0.0')
-     input_data.append(input_value)
-input_df=pd.DataFrame([input_data],columns=X_test.columns)     
-#Make predictions
-if st.button("Predict"):
-    prediction =model.predict(input_df)
-    st.write(f'prediction:{prediction[0]}')
+# Display Accuracy
+st.write(f"Model {accuracy}")
+
+st.header("Real time prediction")
+input_data = []
+for col in x_test.columns:
+    input_value = st.number_input(f'Input for feature {col}',value=0)
+    input_data.append(input_value)
+
+#convert input data to  dataframe
+input_df = pd.DataFrame([input_data], columns=x_test.columns)
+
+# Make prediction
+if st.button("Prediction"):
+    prediction = model.predict(input_df)
+    st.write(f'Prediction: {prediction[0]}')
 
 
 
